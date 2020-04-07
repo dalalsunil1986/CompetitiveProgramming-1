bc8b49df47bec20b59989969b8cc1ab52d834ed1
Salvador <salvadorvalverde0@gmail.com>
2020-04-06 22:57:05 -0600

    Update

--- AtCoder/C159/B.cpp
+++ AtCoder/C159/B.cpp
@@ -4,11 +4,15 @@ using namespace std;
 
 int main(){
     
-    int n, a,b; cin >> n >> a >> b;
+    long long int n, a,b; cin >> n >> a >> b;
 
-    int r = (n / (a+b));
+    long long int r = (n / (a+b)) * a;
 
-  
+    if((n % (a+b)) < a){
+        r += n % (a+b);
+    }else{
+        r += a;
+    }
     cout << r << endl;
     
     return 0;
--- Codeforces/Problems/1300/275A.cpp
+++ Codeforces/Problems/1300/275A.cpp
@@ -9,6 +9,10 @@ int main(){
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
             int v; cin >> v;
+            if(v % 2 == 0){
+
+            }
+            
         }
     }
 }
