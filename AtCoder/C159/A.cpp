4cd47c4bdc141846cd97237a2be89e650bdfb48b
Salvador <salvadorvalverde0@gmail.com>
2020-04-06 18:48:24 -0600

    First

---
+++ AtCoder/C159/A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    string w; cin >> w;
+
+    if(w[0] == w[1] && w[1] == w[2]){
+        cout << "No" << endl;
+    }else{
+        cout << "Yes" << endl;
+    }
+
+}
---
+++ AtCoder/C159/B.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    
+    int n, a,b; cin >> n >> a >> b;
+
+    int r = (n / (a+b));
+
+  
+    cout << r << endl;
+    
+    return 0;
+}
