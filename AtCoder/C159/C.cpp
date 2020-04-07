8020b489e9880acf3dc57f051b121568e9e3bb9e
Salvador <salvadorvalverde0@gmail.com>
2020-04-06 23:17:51 -0600

    Problem B

--- AtCoder/C159/C.cpp
+++ AtCoder/C159/C.cpp
@@ -3,5 +3,15 @@
 using namespace std;
 
 int main(){
+    int a,b; cin >> a >> b;
 
+    for(int i=1;i<1500;i++){
+        if((int)(i*.08) == a && (int)(i*.10) == b){
+            cout << i << endl;
+            return 0;
+        }
+    }
+
+    cout << -1 << endl;
+    
 }
