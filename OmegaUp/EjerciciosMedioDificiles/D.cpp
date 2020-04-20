6572b932159476e8341e4c590dae824bc4e4eb80
Salvador <salvadorvalverde0@gmail.com>
2020-04-19 23:27:15 -0600

    D.cpp

---
+++ OmegaUp/EjerciciosMedioDificiles/D.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int tiempos=0;
+    int total=0;
+
+    int mx=0;
+
+    int v; cin >> v;
+
+    while(v != -1){
+        total += v;
+        tiempos += v;
+         mx = max(mx,tiempos);
+
+        tiempos --;
+        cin >> v;
+    }
+
+    cout << total << " " << mx << endl;
+
+}
