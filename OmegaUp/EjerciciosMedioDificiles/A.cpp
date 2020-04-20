6c1416ba00c87f8aa47aa8fb4427df78f6912a84
Salvador <salvadorvalverde0@gmail.com>
2020-04-20 02:01:38 -0600

    A.cpp

--- OmegaUp/EjerciciosMedioDificiles/A.cpp
+++ OmegaUp/EjerciciosMedioDificiles/A.cpp
@@ -14,18 +14,22 @@ string ron(string w){
             f += w[i];
         }else if(point){
             f += w[i];
-            tp++;
         }else{
             f+= w[i];
         }
-        if(tp >= 2) break;
     }
 
+    if(point){
+
     for(int i=f.size()-1;i>=0;i--){
-        if(w[i] == '0') f.erase(i);
+        if(f[i] == '0') f.erase(i);
         else 
             break; 
     }
+    
+    if(f[f.size()-1] == '.') f.erase(f.size()-1);
+    }
+
     return f;
 }
 
@@ -40,8 +44,8 @@ int main(){
     double max=-1;
     double min=101;
 
-    string amax="";
-    string amin="";
+    vector<string> amax;
+    vector<string> amin;
 
     while(n--){
         cin >> ws;
@@ -60,33 +64,39 @@ int main(){
         else reprobados++;
 
         if(calif > max){
+            amax.clear();
+            amax.push_back(nom);
+
             max = calif;
-            amax = nom;
            
         }else if(calif == max){
-            if(nom < amax){
-                amax = nom;
-            }
+            amax.push_back(nom);
+            
         }
 
         if(calif < min){
+            amin.clear();
+            amin.push_back(nom);
             min = calif;
-            amin = nom;
         
         }else if(calif == min){
-            if(nom < amin){
-                amin = nom;
-            }
+             amin.push_back(nom);
+
         }
     }
 
     cout << fixed << setprecision(2);
 
+
     cout << "PROMEDIO GRUPAL: " << total / ta << endl;
     cout << "ALUMNOS APROBADOS " << aprobados << endl;
-    cout << "AlUMNOS REPROBADOS " << reprobados << endl;
+    cout << "ALUMNOS REPROBADOS " << reprobados << endl;
 
-    cout << amax << " " << ron(to_string(max))<< endl;
-    cout << amin << " " << ron(to_string(min)) << endl;
+    sort(amax.begin(),amax.end());
+    sort(amin.begin(),amin.end());
+
+    cout << amax[0] << " " << ron(to_string(max))<< endl;
+    cout << amin[0] << " " << ron(to_string(min)) << endl;
 
 }
+
