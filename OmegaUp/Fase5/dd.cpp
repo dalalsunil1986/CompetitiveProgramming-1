f3356b537c512bb90eac5481eefd3ac7646bc299
Salvador <salvadorvalverde0@gmail.com>
2020-04-25 01:49:57 -0600

    Update

--- OmegaUp/Fase5/dd.cpp
+++ OmegaUp/Fase5/dd.cpp
@@ -5,28 +5,36 @@ using namespace std;
 int select=0;
 
 vector<int> nums;
-nums.push_back(1);
-
+int i=1;
 int sub(int v){
-
-   while(v > nums.size()){
-       vector<int> tmp = nums,
-   }
    
-    return total;
-    
+
+   int sum =0;
+   for(int i=0;i<v;i++) sum+=nums[i];
+   return sum;
 }
 int getS(int x){
     int d = (x/4.0);
     select =  (int)pow(2,d+2)-x-1;
-    return (pow(2,d+2)/2)*log2(pow(2,d+2))- sub(select)}
+    return (pow(2,d+2)/2)*log2(pow(2,d+2)) - sub(select);
+    }
 int main(){
+    nums.push_back(1);
+
+    while(nums.size() < 100){
+        vector<int> tmp = nums;
+        nums.push_back(pow(2,i));
+        nums.insert(nums.end(),tmp.begin(),tmp.end());
+       i++;
+   }
+
+   //for(int num : nums) cout << num << " "; 
 
     int n; cin >> n;
 
     while(n--){
         int a,b; cin >> a >> b;
-        int r = getS(b) - getS(a-1);
+        int r = getS(b);
         cout << r << endl;
     }
 
