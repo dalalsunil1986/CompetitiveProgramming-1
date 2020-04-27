059b659ce482112bdfc5d200b22a3a12184c2244
Salvador <salvadorvalverde0@gmail.com>
2020-04-26 23:22:01 -0600

    Spoj

--- Spoj/ACODE.cpp
+++ Spoj/ACODE.cpp
@@ -3,29 +3,29 @@
 using namespace std;
 
 int main(){
-    int n; cin >> n;
+    string a; cin >> a;
     
-    while(n != 0){
-        string a = to_string(n);
-        if(a.size() == 0 || a[0] == '0') {
+    while(a != "0"){
+
+        if(a.size() == 0) {
             cout << "0" << endl;
         }
 
-        int dp[a.size()+1] = {};
-        dp[0] = 1;
+        int dp[a.size()] = {};
+        dp[0] = a[0] == '0' ? 0 : 1;
 
         for(int i=1;i<a.size();i++){
             if(a[i] != '0') dp[i] += dp[i-1];
-
-            int d2= stoi(a.substr(i,2));
+            
+            int d2= stoi(a.substr(i-1,2));
 
             if(d2 >= 10 && d2 <= 26){
-                dp[i+1] += dp[i-1];
+                dp[i] += i>=2 ? dp[i-2] : 1;
             }
         }
 
-        cout << dp[a.size()] << endl;
-        cin >> n;
+        cout << dp[a.size()-1] << endl;
+        cin >> a;
 
     }
 }
