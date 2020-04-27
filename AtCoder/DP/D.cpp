2fed8f22d20deeff90cc8da0a0eb37e334b8a4b8
Salvador <salvadorvalverde0@gmail.com>
2020-04-27 02:44:17 -0600

    D.cpp

---
+++ AtCoder/DP/D.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+#define W first
+#define V second
+#define ll long long int
+
+using namespace std;
+
+int main(){
+    int n,w; cin >> n >> w;
+
+    vector<pair<int,int>> v;
+
+    for(int i=0;i<n;i++){
+        int a,b; cin >> a >> b;
+        v.push_back({a,b});
+    }
+
+    ll dp[n+1][w+1]; memset(dp,0,sizeof(dp));
+
+    for(ll i=1; i<=n;i++){
+        for(ll j=1;j<=w;j++){
+            if(v[i-1].W <= j){
+                dp[i][j] = max(dp[i-1][j], dp[i-1][j-v[i-1].W]+v[i-1].V);
+            }else{
+                dp[i][j] = dp[i-1][j];
+            }
+        }
+    }
+    
+    cout << dp[n][w] << endl;
+}
